b636a0a0bbb70357b4ddcc066b819edf5f7aacb3
kortunovaa <kortunova2006@mail.ru>
2026-02-16 18:06:00 +0400

    добавила исходный текст и найденный вручную словарь

---
+++ Task2_lab1.py
@@ -0,0 +1,47 @@
+cipher_text = """PE!WFxZ!CFU!9Fn!CAS$nFSZtI!IOZ>hIKn!9AAW$ZU9tnWZEAnI!AInFCLnx
+ZFYQVAxIZA9FEnIhEZEZ=!WBEIZKCAChx
+9AEZV9BWnZVCFKE!9PCnFSZU9=ZU9hIOAxIZU!9B!CVVxZEhEZ3CLhxZtn9MxZ9MVCAWn-ZU9hO9PCnIhSZEZOCFnCPEn-ZIB9ZOCUWFnEn-ZPE!WF
+tn9MxZOC8EnEn-FSZ9nZPE!WF9PZAI9MG9=EV9ZEFU9h-O9PCnZCAnEPE!WFA9IZU!9B!CVVA9IZ9MIFUItIAEIZK9n9!9IZV9JInZ9MAC!WJ
+EPCn-ZEZW=ChSn-ZPE!WFxZEOZFEFnIVx
+nCKJIZPCJA9ZAIZ9nK!xPCn-ZU9=9O!EnIh-AxIZUEFVCZEZFFxhKEZAIZOCB!WJCnZ3CLhxZEOZAIAC=QJAxGZEFn9tAEK9PZEZ!IBWhS!A9Z9MA9PhSnZ9UI!CRE9AAW$ZFEFnIVWZEZU!Eh9JIAES
+IFhEZPxZU9=9O!IPCInIZtn9ZPCdZK9VU-$nI!ZEhEZV9MEhA9IZWFn!9LFnP9ZOC!CJIA9ZPE!WF9VZAIVI=hIAA9Z9M!CnEnIFZKZFUIREChEFnWZU9ZEA39!VCRE9AA9LZMIO9UCFA9FnEZEhEZEFU9h
+-
+OWLnIZCAnEPE!WFAW$ZU!9B!CVVWZ=hSZFKCAE!9PCAESZEZ9tEFnKE
+ZFEFnIVx"""
+
+key = {
+    'n': 'т',
+    '-': 'ь',
+    'h': 'л',
+    'E': 'и',
+    'g': 'о',
+    'O': 'з',
+    'Z': ' ',
+    'U': 'п',
+    '=': 'д',
+    '!': 'р',
+    'I': 'е',
+    'A': 'н',
+    'x': 'ы',
+    'F': 'с',
+    'V': 'м',
+    'W': 'у',
+    'R': 'ц',
+    'K': 'к',
+    '$': 'ю',
+    'C': 'а',
+    'P': 'в',
+    'S': 'я',
+    'M': 'б',
+    'B': 'г',
+    't': 'ч',
+    '8': 'щ',
+    '>': 'э',
+    'L': 'й',
+    'Y': 'ъ',
+    'Q': 'ё',
+    '3': 'ф',
+    'G': 'х',
+    'J': 'ж',
+    '9': 'о'  
+}
