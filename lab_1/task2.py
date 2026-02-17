d2f1e21b940112b22709cb315c224d5ee1043cd9
klok234 (Dmitriy) <dolzhikovdima1@gmail.com>
2026-02-17 12:05:32 +0400

    Added base struct for frequency analized decode

--- lab_1/task2.py
+++ lab_1/task2.py
@@ -1,16 +1,14 @@
-def main() -> None:
+def get_freq_file(text: str) -> None:
     """
-    Docstring for main
+    Docstring for get_freq_file
+    return table frequency of chars to file
     """
     chars = [chr(x) for x in range(256)]
     size = 0
-    text = ""
     freq = dict.fromkeys(chars, 0)
-    with open("cod11.txt", "r") as f:
-        text = f.read()
-        for i in text:
-            freq[i] += 1
-            size += 1
+    for i in text:
+        freq[i] += 1
+        size += 1
     freq = {
         k: v
         for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)
@@ -20,6 +18,35 @@ def main() -> None:
     with open("freq_text.txt", "w") as f:
         for ch, fr in freq.items():
             f.write(ch + " " + str(fr / size) + "\n")
+
+
+def decode_text(text: str) -> None:
+    """
+    Docstring for decode_text
+
+    :param text: Encoded text to decode
+    :type text: str
+    """
+    replaced_table = {"Z": " "}
+
+    for i, j in replaced_table.items():
+        text = text.replace(i, j)
+
+    print(text)
+    with open("decode.txt", "w", encoding="utf-8") as f:
+        f.write(text)
+
+
+def main() -> None:
+    """
+    Docstring for main
+    """
+    text = ""
+    with open("cod11.txt", "r") as f:
+        text = f.read()
+
+    get_freq_file(text)
+    decode_text(text)
 
 
 if __name__ == "__main__":
