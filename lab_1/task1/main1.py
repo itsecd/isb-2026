906639ac868e383351c1794ab58dde17cedc8513
Anna <anutka893706@gmail.com>
2026-02-17 15:30:01 +0400

    Added encoding function

--- lab_1/task1/main1.py
+++ lab_1/task1/main1.py
@@ -5,12 +5,31 @@ def read(file: str) -> str:
 
     res = ""
     try:
-        with open(file) as f:
+        with open(file, encoding="utf-8") as f:
             res = f.read()
             return res
     except Exception as e:
         print(e)
         exit(-1)
+
+
+def encode(text: str, key: str) -> str:
+    table = []
+    rows = len(text) // 10 + 1
+    for i in range(rows):
+        table.append([""]*10)
+
+    for i in range(rows):
+        for j in range(10):
+            if i * 10 + j >= len(text):
+                break
+            table[i][j] = text[i * 10 + j]
+    text = ""
+    for i in key:
+        for j in range(rows):
+            text += table[j][int(i)]
+
+    return text
 
 
 def main() -> None:
@@ -25,7 +44,7 @@ def main() -> None:
     text = encode(text, key)
 
     try:
-        with open(sys.argv[3], "w") as f:
+        with open(sys.argv[3], "w", encoding="utf-8") as f:
             f.write(text)
 
     except Exception as e:
