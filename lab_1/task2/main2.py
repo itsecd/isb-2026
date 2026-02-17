4f988ce7b0596838a49bf2f0fe4fe2738a365a36
Anna <anutka893706@gmail.com>
2026-02-17 16:44:23 +0400

    Added decode function

--- lab_1/task2/main2.py
+++ lab_1/task2/main2.py
@@ -1,8 +1,23 @@
-def main() -> None:
-    text = ""
-    with open("cod10.txt", encoding="utf-8") as f:
-        text = f.read()
+def decode(text: str) -> str:
+    """
+    Decoding text
+    """
+    dictionary = {"Y": " ", "Ё": "о", "К":"е", "Я":"и", "s": "л", "Й":"т",
+                  "U":"н", "Д":"к", "i":"ь", "ю":"р", "7":"п", "Q":"с",
+                  "И":"в", "R":"а", "г":"б", "@":"з", "Ж":"я", "О":"ш",
+                  "Т":"ж", "F":"э", "J":"й", "G":"ч", "Р":"ю", "1":"ы",
+                  "у":"ъ", "%":"д", "3":"м", "=":"г", "Z":"ц", "Х":"х", "N":"ф", "П":"щ", "<":"у"}
 
+    for i, j in dictionary.items():
+        text = text.replace(i, j)
+
+    return text
+
+
+def analyzer(text: str) -> str:
+    """
+    Get frequency of chars 
+    """
     chars = [ord(i) for i in set(text)]
 
     frequency = {chr(i): 0 for i in chars}
@@ -21,6 +36,20 @@ def main() -> None:
     with open("freq.txt", "w", encoding="utf-8") as f:
         for ch, fr in frequency.items():
             f.write(ch + " " + str(fr) + "\n")
+
+
+def main() -> None:
+    """
+    Main function.
+    """
+    text = ""
+    with open("cod10.txt", encoding="utf-8") as f:
+        text = f.read()
+    text = decode(text)
+    print(text)
+
+    with open("decode.txt", "w", encoding="utf-8") as f:
+        f.write(text)
 
 
 if __name__ == "__main__":
