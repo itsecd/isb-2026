71d09ae8821407d3fff7c9fcd613160d6f0f2fed
student <student@ssau.ru>
2026-02-18 11:25:50 +0400

    начало task 2

--- lab_1/task2.py
+++ lab_1/task2.py
@@ -1,6 +1,25 @@
+def mapper(text: str) -> str:
+    map = { "О": " "}
+    res = ""
+    for c in text:
+        if c in map.keys():
+            res += map[c]
+        else:
+            res += c
+    return res
+
+
 def main() -> None:
     with open("lab_1/cod4.txt", encoding="utf-8", mode="r") as f:
         data = f.read()
+    
+    char_data = dict.fromkeys(set(data), 0)
+    for c in data:
+        char_data[c] += 1
+    print(*sorted(list(char_data.items()),key=lambda x: x[1], reverse=True)[0:10], "...")
+
+    data_map = mapper(data)
+    print(data_map)
     
 
 if __name__ == "__main__":
