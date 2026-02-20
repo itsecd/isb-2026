f7458eb4d0059f45006538e19a19b04f8910b734
bbtoji <mrmagistrr@gmail.com>
2026-02-21 00:15:27 +0400

    adding decrypt and encrypt functions

--- lab_1/vigenere.py
+++ lab_1/vigenere.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 
+def encrypt_vigenere(plaintext, key):
+    plaintext = plaintext.upper()
+    key = key.upper()
+    ciphertext = ""
+    key_index = 0
+
+    for char in plaintext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - ord('A')
+            encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += encrypted_char
+            key_index += 1
+        else:
+            ciphertext += char
+    return ciphertext
+
+def decrypt_vigenere(ciphertext, key):
+    ciphertext = ciphertext.upper()
+    key = key.upper()
+    plaintext = ""
+    key_index = 0
+
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - ord('A')
+            decrypted_char = chr((ord(char) - ord('A') - shift + 26) % 26 + ord('A'))
+            plaintext += decrypted_char
+            key_index += 1
+        else:
+            plaintext += char
+    return plaintext
+
 def main():
     pass
 
