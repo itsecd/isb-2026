e8628aeb51712414c90dbc0539239852be949d78
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2026-02-22 18:34:06 +0400

    added read_text func

--- lab_1/encryption.py
+++ lab_1/encryption.py
@@ -11,11 +11,23 @@ def argument_parsing() -> list[str]:
     parser.add_argument('output_file_name', type=str,
                         help='path to output file')
     args = parser.parse_args()
-    return [args.input_file_name, args.enctyption_key, args.output_file_name]
+    return [args.input_file_name, args.encryption_key, args.output_file_name]
+
+
+def read_text(input_file: str) -> str:
+    with open(input_file, 'r', encoding='utf-8') as file:
+        return file.read()
 
 
 def main():
     '''Reads input_file and enctypts it to output_file using some key'''
+    args = argument_parsing()
+    input_file = args[0]
+    key = args[1]
+    output_file = args[2]
+
+    text = read_text(input_file)
+    print(text)
 
 
 if __name__ == '__main__':
