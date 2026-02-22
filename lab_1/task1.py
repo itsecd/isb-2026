4b8b984f08bea7ba79b30b081fb06149d2533b95
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2026-02-22 21:38:29 +0400

    add encode function

--- lab_1/task1.py
+++ lab_1/task1.py
@@ -1,1 +1,45 @@
-print("Hello world")
+import argparse
+
+import pandas as pd
+
+def read_text(filename: str) -> str:
+    """ function for read from file """
+    try:
+        with open(filename, encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found")
+        exit(2)
+    except PermissionError:
+        print(f"Premision denied")
+        exit(2)
+    except Exception as e:
+        print(e)
+        exit(2)
+
+    
+def encode_polibius(text: str, key: pd.DataFrame) -> str:
+    """ encode text function """
+    text = text.upper()
+    rows, cols = key.shape
+    map = {}
+    for i in range(rows):
+        for j in range(cols):
+            cell = key.iloc[i, j]
+            if cell and str(cell).strip():
+                map[cell] = f"{i+1}{j+1}"
+    result = []
+    for char in text:
+        if char in map:
+            result.append(map[char])
+        elif char == ' ':
+            result.append(' ')
+        else:
+            result.append(char)
+    return ' '.join(result)
+
+def main() -> None:
+    print()    
+
+if __name__ == "__main__":
+    main()
