e3e672fd50ea26c015ae45f58cd66f0e1aa31ec6
clay <clayworks1337@gmail.com>
2026-02-24 15:41:45 +0400

    Add writing freq in txt file

--- lab_1/task2/code.py
+++ lab_1/task2/code.py
@@ -42,21 +42,57 @@ def parse_args() -> argparse.Namespace:
                         help="Path where key is located")
     return parser.parse_args()
 
+def create_substitution_table(key: str, is_reversed:bool = False) -> set:
+    """
+    Create substitution table between alphabets
+    """
+    alphabet = "ЪЭЩШЦБЮЙФХГЧЬЫЗПКЯДЛМВРТСНАЕИО "
+    table = {}
+    if is_reversed == True:
+        for i in range(len(alphabet)):
+            table[alphabet[i]] = key[i]
+    else:
+        for i in range(len(alphabet)):
+            table[key[i]] = alphabet[i]
+
+    return table
+
+def create_freq_analysis(text: str) -> str:
+    table = {}
+    for ch in text:
+        if ch not in table:
+            table[ch] = 1
+        else:
+            table[ch] += 1
+
+    total_chars = sum(table.values()) 
+    freq_table = {k: v / total_chars for k, v in table.items()}
+
+    sorted_table = dict(sorted(freq_table.items(), key=lambda item: item[1], reverse=True))
+
+    result = '\n'.join(f"{k} = {v:.6f}" for k, v in sorted_table.items())
+    return result
+
+def encrypting_text(text: str, table: set) -> str:
+    """
+    Encrypting or decrypting text with substitution table
+    """
+    result = ""
+    
+    for ch in text:
+        if ch in table:
+            result += table[ch]
+        else:
+            result += ch
+    return result
+
 
 def main() -> None:
     args = parse_args()
     encrypted_text = read_file(args.read_file).replace("\n", "")
-    alphabet = "".join(set(encrypted_text)).strip()
-    write_file("alphabet.txt", alphabet)
 
-    table = {}
-    for ch in alphabet:
-        table[ch] = 0
-    for ch in encrypted_text:
-        table[ch]+=1
-
-    sorted_table = dict(sorted(table.items(), key=lambda item: item[1]))
-    print(sorted_table)
+    table = create_freq_analysis(encrypted_text)
+    write_file(args.write_file, table)
 
 if __name__ == "__main__":
     main()
