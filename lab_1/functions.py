182562d02fd8b5694405dd5ff33a4ad96b913f46
EnrayMaker <250-5555@mail.ru>
2026-03-01 01:20:01 +0400

    Cosmetic changes №2

--- lab_1/functions.py
+++ lab_1/functions.py
@@ -4,24 +4,23 @@
 from constants import RU_ALPHABET
 
 
-def vigenere_encrypt(text, key):
+def vigenere_encrypt(text: str, key: str) -> str:
     """Vigenere cipher"""
 
 
-    encrypted_text = []
-    key_length = len(key)
+    encrypted_text: list[str] = []
+    key_length: int = len(key)
     text = text.upper()
     
     for i in range(len(text)):
-        current_char = text[i]
-
-        char_index = RU_ALPHABET.find(current_char)
+        current_char: str = text[i]
+        char_index: int = RU_ALPHABET.find(current_char)
          
         if char_index != -1:
-            key_char = key[i % key_length]
-            key_index = RU_ALPHABET.find(key_char.upper())
+            key_char: str = key[i % key_length]
+            key_index: int = RU_ALPHABET.find(key_char.upper())
 
-            new_index = (char_index + key_index) % 33
+            new_index: int = (char_index + key_index) % 33
             encrypted_text.append(RU_ALPHABET[new_index])
         else:
             encrypted_text.append(current_char)
@@ -29,12 +28,15 @@ def vigenere_encrypt(text, key):
     return ''.join(encrypted_text)
 
 
-def create_key():
+def create_key() -> str:
     """Creating encryption key"""
     return "ПЕПС"
 
 
-def write_to_files(original_text, encrypted_text, key):
+def write_to_files(
+        original_text: str,
+        encrypted_text: str,
+        key: str) -> None:
     """Function for writing to files"""
 
     with open('encrypted_text.txt', 'w', encoding='utf-8') as file:
@@ -48,14 +50,14 @@ def write_to_files(original_text, encrypted_text, key):
         file.write(f"Длина ключа: {len(key)}\n")
 
 
-def main():
-    plain_text = """Власти готовят финальный удар по Telegram
+def main() -> None:
+    plain_text: str = """Власти готовят финальный удар по Telegram
 Российские власти определились со сроками полной блокировки мессенджера Telegram. По данным источников, знакомых с обсуждениями в профильных ведомствах, отключение планируют провести в начале апреля. Два источника, близких к Кремлю, называют это окончательным решением. Главная причина - участившиеся случаи вербовки граждан и несовершеннолетних для противоправной деятельности.
 В отношении основателя Telegram Павла Дурова расследуется уголовное дело по статье о содействии террористической деятельности. Ч. 1.1 ст. 205.1 УК России предусматривает от восьми лет до пожизненного лишения свободы. Сам Дуров связал это с попыткой подавить право на частную жизнь и свободу слова.
 Блокировке предшествовали постепенные ограничения работы мессенджера. С 10 февраля власти начали замедлять Telegram, а ранее ограничили голосовые звонки, объясняя это защитой граждан от мошенничества и вовлечения в противоправную деятельность."""
     
     plain_text = ' '.join(plain_text.split())
     
-    key = create_key()
-    encrypted_text = vigenere_encrypt(plain_text, key)
+    key: str = create_key()
+    encrypted_text: str = vigenere_encrypt(plain_text, key)
     write_to_files(plain_text, encrypted_text, key)
--- lab_1/main.py
+++ lab_1/main.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Program entry point"""
+
 import functions
 
 
