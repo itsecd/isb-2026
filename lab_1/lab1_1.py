8c8eb19ac25a982c611b0f523d0272d7a9119e13
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2026-02-28 21:17:45 +0400

    добавил описание и типизацию  функций

--- lab_1/lab1_1.py
+++ lab_1/lab1_1.py
@@ -1,7 +1,9 @@
 import argparse
 
 def parse_arguments():
-    """Настраивает парсер и возвращает аргументы командной строки."""
+    """
+    парсинг командной строки
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("file_a", help="Путь к тексту")
     parser.add_argument("file_b", help="Путь к ключу")
@@ -10,16 +12,25 @@ def parse_arguments():
     return parser.parse_args()
 
 def load_file(path:str)-> str:
+    """
+    чтение текста из файла
+    """
     with open(path, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
-def save_file(path:str, text:str):
+def save_file(path:str, text:str)-> None:
+    """
+    сохранение текста в файл
+    """
     with open(path, "w", encoding="utf-8") as f:
         f.write(text)
     
 
-def clear_key(key:str, alfovit: str):
+def clear_key(key:str, alfovit: str) -> list:
+    """
+    очистка ключа от ненужных символов
+    """
     clean_key = []
     
     for letter in key:
@@ -30,7 +41,7 @@ def clear_key(key:str, alfovit: str):
 
 def vigener_cipher(text: str, key: str, alfovit: str, mode:bool) -> str:
     """
-    
+    в зависимости от mode либо шифрует, либо дешифрует текст
     """
     result = ""
     key = clear_key(key, alfovit)
@@ -64,7 +75,10 @@ def vigener_cipher(text: str, key: str, alfovit: str, mode:bool) -> str:
 
 
 
-def main():
+def main() -> None:
+    """
+    основная функция программы
+    """
     args = parse_arguments()
     
     text = load_file(args.file_a)
@@ -80,5 +94,7 @@ def main():
     result = vigener_cipher(text, key, alfovit, mode)
     save_file(args.file_c, result)
     
-    
-    return 0
+
+
+if __name__ == "__main__":
+    main()
