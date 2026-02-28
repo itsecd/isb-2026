1d4c1e260489463cf3de567f9f886e36a6391bc0
EnrayMaker <250-5555@mail.ru>
2026-03-01 00:20:07 +0400

    Add some fixes in algorithm

--- lab_1/functions.py
+++ lab_1/functions.py
@@ -1,12 +1,39 @@
-def vigenere_encrypt(plain_text, key):
+def vigenere_encrypt(text, key):
     """Vigenere cipher"""
+
+    ru_upper = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+
     encrypted_text= []
     key_length = len(key)
-    key_as_int = [ord(i) for i in key]
+    #key_as_int = [ord(i) for i in key]
+
+    for i in range(len(text)):
+        current_char = text[i]
+        
+        char_index=-1
+        for j in range(len(ru_upper)):
+            if ru_upper[j] == current_char:
+                char_index = j
+                break
+        if char_index != -1:
+            key_char = key[i % key_length]
+            key_index = -1
+            for j in range(len(ru_upper)):
+                if ru_upper[j] == key_char:
+                    key_index = j
+                    break
+
+
+            new_index = (char_index + key_index) % 33
+            encrypted_text += ru_upper[new_index]
+        else:
+            encrypted_text += current_char
+
+    return ''.join(encrypted_text)
 
 
 def create_key():
-    """creating encryption key"""
+    """Creating encryption key"""
     key = "ПЕПС"
     return key
 
@@ -17,7 +44,7 @@ def write_to_files(original_text, encrypted_text, key):
     with open('encrypted_text.txt', 'w', encoding='utf-8') as file:
         file.write(encrypted_text)
     
-    with open('original_text', 'w', encoding='utf-8') as file:
+    with open('original_text.txt', 'w', encoding='utf-8') as file:
         file.write(original_text)
  
     with open('encryption_key.txt', 'w', encoding='utf-8') as file:
@@ -25,7 +52,7 @@ def write_to_files(original_text, encrypted_text, key):
         file.write(f"Длина ключа: {len(key)}\n")
 
 def main():
-    plain_text = ""
+    plain_text = "вавВВА"
     plain_text = ' '.join(plain_text.split())
     key = create_key()
 
