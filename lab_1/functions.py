52826098fd16453105564defe4e0c97a1b5b66db
EnrayMaker <250-5555@mail.ru>
2026-03-01 01:10:08 +0400

    Some cosmetic fixes. Make PEP-8 friendly

--- lab_1/functions.py
+++ lab_1/functions.py
@@ -1,41 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Functions"""
+
+from constants import RU_ALPHABET
+
+
 def vigenere_encrypt(text, key):
     """Vigenere cipher"""
 
-    ru_upper = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
-    encrypted_text= []
+    encrypted_text = []
     key_length = len(key)
-    #key_as_int = [ord(i) for i in key]
-
+    text = text.upper()
+    
     for i in range(len(text)):
         current_char = text[i]
-        
-        char_index=-1
-        for j in range(len(ru_upper)):
-            if ru_upper[j] == current_char:
-                char_index = j
-                break
+
+        char_index = RU_ALPHABET.find(current_char)
+         
         if char_index != -1:
             key_char = key[i % key_length]
-            key_index = -1
-            for j in range(len(ru_upper)):
-                if ru_upper[j] == key_char:
-                    key_index = j
-                    break
-
+            key_index = RU_ALPHABET.find(key_char.upper())
 
             new_index = (char_index + key_index) % 33
-            encrypted_text += ru_upper[new_index]
+            encrypted_text.append(RU_ALPHABET[new_index])
         else:
-            encrypted_text += current_char
+            encrypted_text.append(current_char)
 
     return ''.join(encrypted_text)
 
 
 def create_key():
     """Creating encryption key"""
-    key = "ПЕПС"
-    return key
+    return "ПЕПС"
 
 
 def write_to_files(original_text, encrypted_text, key):
@@ -51,11 +47,15 @@ def write_to_files(original_text, encrypted_text, key):
         file.write(f"Ключ шифрования: {key}\n")
         file.write(f"Длина ключа: {len(key)}\n")
 
+
 def main():
-    plain_text = "вавВВА"
+    plain_text = """Власти готовят финальный удар по Telegram
+Российские власти определились со сроками полной блокировки мессенджера Telegram. По данным источников, знакомых с обсуждениями в профильных ведомствах, отключение планируют провести в начале апреля. Два источника, близких к Кремлю, называют это окончательным решением. Главная причина - участившиеся случаи вербовки граждан и несовершеннолетних для противоправной деятельности.
+В отношении основателя Telegram Павла Дурова расследуется уголовное дело по статье о содействии террористической деятельности. Ч. 1.1 ст. 205.1 УК России предусматривает от восьми лет до пожизненного лишения свободы. Сам Дуров связал это с попыткой подавить право на частную жизнь и свободу слова.
+Блокировке предшествовали постепенные ограничения работы мессенджера. С 10 февраля власти начали замедлять Telegram, а ранее ограничили голосовые звонки, объясняя это защитой граждан от мошенничества и вовлечения в противоправную деятельность."""
+    
     plain_text = ' '.join(plain_text.split())
+    
     key = create_key()
-
     encrypted_text = vigenere_encrypt(plain_text, key)
-
     write_to_files(plain_text, encrypted_text, key)
