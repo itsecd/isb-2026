2eacefa314e4f32ff805b31f653f17ff4bf19585
EnrayMaker <250-5555@mail.ru>
2026-03-02 00:41:38 +0400

    Add some QoL functions

--- lab_2/sequence/nist_test_sequence.py
+++ lab_2/sequence/nist_test_sequence.py
@@ -1,11 +1,18 @@
 import math
+import os
 
 
-def frequency_test(sequence: str):
+def load_sequence(file_path: str) -> str:
+    """Open file with sequences and cleaning spaces"""
+    if not os.path.exists(file_path):
+        return ""
+    with open(file_path, 'r') as f:
+        return f.read().strip()
+
+def frequency_test(sequence: str) -> str:
     """Frequency test function"""
 
     n: int = len(sequence)
-
     s_n: int = 0
     for bit in sequence:
         if bit == '1':
@@ -14,21 +21,20 @@ def frequency_test(sequence: str):
             s_n -= 1
 
     s_obs: float = abs(s_n) / math.sqrt(n)
+    res: str = f"Test - 1: Frequency \n"
+    res += f"Sum (S_n): {s_n}\n"
+    res += f"Stastics (S_obs): {s_obs:.4f}\n"
+    res += "P-value: erfc(S_obs / sqrt(2))\n"
+    return res
 
-    print(f"Sum (S_n): {s_n}")
-    print(f"Stastics (S_obs): {s_obs:.4f}")
-    print("erfc(S_obs / sqrt(2)) = ... (online calc ;( )")
-    return s_obs
-
-def runs_test(sequence: str):
+def runs_test(sequence: str) -> str:
     """Runs test"""
     n: int = len(sequence)
     ones_count: int = sequence.count('1')
     pi: float = ones_count / n
 
     if abs(pi - 0.5) >= (2 / math.sqrt(n)):
-        print("Test failed")
-        return 0
+        return("Test failed")
 
     v_n = 1
     for i in range(n - 1):
@@ -38,13 +44,13 @@ def runs_test(sequence: str):
     numerator: float = abs(v_n - 2 * n * pi * (1 - pi))
     denominator: float = 2 * math.sqrt(2 * n) * pi * (1 - pi)
     s_obs: float = numerator / denominator
+    res: str = f"Test - 2: Runs \n"
+    res += f"V_n: {v_n}\n"
+    res += f"S_obs: {s_obs:.4f}\n"
+    return res
 
-    print(f"V_n: {v_n}")
-    print(f"S_obs: {s_obs:.4f}")
-    return s_obs
-
-def longest_run_test(sequence: str):
-    """"""
+def longest_run_test(sequence: str) -> str:
+    """Longest run test funcion XD"""
 
     n: int = len(sequence)
     m = 8
@@ -69,12 +75,49 @@ def longest_run_test(sequence: str):
     x2_obs = 0 
     for i in range(4):
         x2_obs += ((v[i] - n_blocks * pi[i])**2) / (n_blocks * pi[i])
+    res = f"Test - 3: Longest runs\n"
+    res += f"[<=1, 2, 3, >=4]: {v}\n"
+    res += f"Chi-squeare: {x2_obs:.4f}"
+    return res
 
-    print("[<=1, 2, 3, >=4]: {v}")
-    print("chi_square: {x2_obs:.4f}")
-    print("Calc: df=3")
-    return x2_obs
+def run_full_tests():
+    base_path = "../generate/results/"
+    targets = {
+            "C++": "cpp_result",
+            "Python": "python_result",
+            "Java": "java_result"
+    }
 
-frequency_test("10110101110111001000000101110100101001110000000100100010011000000101011000000001101011101010011111010110001101111101001110100100")
-runs_test("10110101110111001000000101110100101001110000000100100010011000000101011000000001101011101010011111010110001101111101001110100100")
-longest_run_test("10110101110111001000000101110100101001110000000100100010011000000101011000000001101011101010011111010110001101111101001110100100")
+    report_lines = []
+
+    for lang, filename in targets.items():
+        full_path = os.path.join(base_path, filename)
+        seq = load_sequence(full_path)
+        
+        header = f"\n{'='*10} Analysis {lang} ({filename}) {'='*10}\n"
+        print(header)
+        report_lines.append(header)
+        
+        if not seq:
+            msg = f"Error file {filename} not found.\n"
+            print(msg)
+            report_lines.append(msg)
+            continue
+
+        results = [
+            frequency_test(seq),
+            runs_test(seq),
+            longest_run_test(seq)
+        ]
+        
+        for r in results:
+            print(r)
+            report_lines.append(r)
+
+    with open("nist_report.txt", "w", encoding="utf-8") as f:
+        f.writelines(report_lines)
+    print("\n[OK] Results sace in nist_report.txt")
+
+
+if __name__ == "__main__":
+    run_full_tests()
