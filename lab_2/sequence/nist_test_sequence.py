4157200bb03718199548fd594a9d8c8646027305
EnrayMaker <250-5555@mail.ru>
2026-03-02 00:07:01 +0400

    Add runs test func

--- lab_2/sequence/nist_test_sequence.py
+++ lab_2/sequence/nist_test_sequence.py
@@ -20,4 +20,27 @@ def frequency_test(sequence: str):
     print("erfc(S_obs / sqrt(2)) = ... (online calc ;( )")
     return s_obs
 
+def runs_test(sequence: str):
+    """Runs test"""
+    n: int = len(sequence)
+    ones_count: int = sequence.count('1')
+    pi: float = ones_count / n
+
+    if abs(pi - 0.5) >= (2 / math.sqrt(n)):
+        print("Test failed")
+        return 0
+
+    v_n = 1
+    for i in range(n - 1):
+        if sequence[i] != sequence[i+1]:
+            v_n += 1
+
+    numerator: float = abs(v_n - 2 * n * pi * (1 - pi))
+    denominator: float = 2 * math.sqrt(2 * n) * pi * (1 - pi)
+    s_obs: float = numerator / denominator
+
+    print(f"V_n: {v_n}")
+    print(f"S_obs: {s_obs:.4f}")
+    return s_obs
+
 
