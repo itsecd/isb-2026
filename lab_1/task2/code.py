cb86f9b14e16abd4806ba3f1f0c7f3558b2b23eb
clay <clayworks1337@gmail.com>
2026-03-02 21:47:07 +0400

    Add answer

--- lab_1/task2/code.py
+++ lab_1/task2/code.py
@@ -32,6 +32,15 @@ def parse_args() -> argparse.Namespace:
 def get_freq_order(text: str) -> str:
     return "".join(ch for ch, x in Counter(text).most_common())
 
+def write_freq_order(text: str) -> str:
+    total = len(text)
+    counter = Counter(text)
+    lines = []
+    for ch, count in counter.most_common():
+        freq = count / total
+        lines.append(f"{ch} = {freq:.6f}")
+    return "\n".join(lines)
+
 def build_table(cipher_chars: str) -> dict:
     ru_freq_order = " ОИЕАНТСРВМЛДЯКПЗЫЬУЧЖГХФЙЮБЦШЩЭЪ"
     return {cipher_chars[i]: ru_freq_order[i] for i in range(min(len(cipher_chars), len(ru_freq_order)))}
@@ -39,7 +48,7 @@ def build_table(cipher_chars: str) -> dict:
 def decrypt(text: str, table: dict) -> str:
     return "".join(table.get(c, c) for c in text)
 
-def show_preview(text: str, table: dict, limit:int = 300) -> None:
+def show_preview(text: str, table: dict, limit:int = 500) -> None:
     dec = decrypt(text, table)
     print(dec[:limit])
 
@@ -54,13 +63,13 @@ def swap_chars(table: dict, char1: str, char2: str) -> dict:
             key2 = k
 
     if key1 is None or key2 is None:
-        print("Одна из букв не найдена в таблице")
+        print("One of the letters is not found in the table")
         return table
 
     table[key1], table[key2] = table[key2], table[key1]
     return table
 
-def manual_fixes(text: str, table: dict, limit:int = 300) -> dict:
+def manual_fixes(text: str, table: dict) -> dict:
     print("Automatic replacement")
     show_preview(text, table)
     print("\nEnter exchange: code=letter (for example: P=M). Empty string — finish\n")
@@ -84,7 +93,9 @@ def save_key(table: dict, path: str):
 
 def main():
     args = parse_args()
-    text = read_file(args.input)
+    text = read_file(args.input).replace("\n", "")
+
+    write_file("freq_text_alph.txt", write_freq_order(text))
 
     order = get_freq_order(text)
     table = build_table(order)
@@ -94,6 +105,7 @@ def main():
     decrypted_text = decrypt(text, table_key)
 
     write_file(args.output, decrypted_text)
+
 
     print("Decrypted text:")
     print(decrypted_text)
