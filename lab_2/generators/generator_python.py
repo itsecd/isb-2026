084b0f9780535177b66139f61292e40293674225
Anna <anutka893706@gmail.com>
2026-03-05 16:03:07 +0400

    Added generators

---
+++ lab_2/generators/generator_python.py
@@ -0,0 +1,14 @@
+import random
+
+
+def main() -> None:
+    res = ""
+    for i in range(128):
+        res += str(random.randint(0, 1))
+
+    with open("python.txt", "w") as f:
+        f.write(res)
+    
+
+if __name__ == "__main__":
+    main()
