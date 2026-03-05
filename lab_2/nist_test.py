a763558b9fe4c1357788e0aff7e527799737e945
Anna <anutka893706@gmail.com>
2026-03-05 16:05:19 +0400

    Added test2

--- lab_2/nist_test.py
+++ lab_2/nist_test.py
@@ -14,3 +14,17 @@ def test_frequency(text: str) -> float:
     Sn *= 1 / (len(text)) ** 0.5
 
     return math.erfc(Sn / 2 ** 0.5)
+
+
+def test_identical_bits(text: str) -> float:
+    """
+    NIST test for a sequence of identical bits.
+    Return P value for this text
+    """
+    ksi = text.count("1") / len(text)
+    if not (abs(ksi - 0.5) < 2 / (len(text)) ** 0.5):
+        return 0
+    Vn = text.count("01") + text.count("10")
+    num = abs(Vn - 2 * len(text) * ksi * (1 - ksi))
+    denom = 2 * ((2 * len(text)) ** 0.5) * ksi * (1 - ksi)
+    return math.erfc(num / denom)
