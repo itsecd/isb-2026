5ef96999dbfffd7e4d4d43542c005bdc0fc11562
clay <clayworks1337@gmail.com>
2026-03-10 20:45:10 +0400

    Fixes

--- lab_2/nist_tests.py
+++ lab_2/nist_tests.py
@@ -1,5 +1,5 @@
 import math
-from typing import Tuple, List
+from scipy.special import gammaincc
 
 
 def read_sequence(path: str) -> str:
@@ -20,14 +20,14 @@ def frequency_test(bits: str) -> float:
     return p_value
 
 
-def runs_test(bits: str) -> float:
+def runs_test(bits: str):
     """Runs Test"""
     n = len(bits)
 
     pi = bits.count("1") / n
 
     if abs(pi - 0.5) >= (2 / math.sqrt(n)):
-        return 0.0
+        return "Test isn't working"
 
     runs = 1
 
@@ -35,15 +35,15 @@ def runs_test(bits: str) -> float:
         if bits[i] != bits[i - 1]:
             runs += 1
 
-    numerator = abs(runs - 2 * n * pi * (1 - pi))
+    numerator = abs(runs - (2 * n * pi * (1 - pi)))
     denominator = 2 * math.sqrt(2 * n) * pi * (1 - pi)
 
     p_value = math.erfc(numerator / denominator)
 
     return p_value
+    
 
-
-def longest_run_test(bits: str) -> Tuple[float, List[int]]:
+def longest_run_test(bits: str) -> Tuple[float, float, List[int]]:
     """Longest Run of Ones Test"""
     block_size = 8
     num_blocks = len(bits) // block_size
@@ -80,21 +80,24 @@ def longest_run_test(bits: str) -> Tuple[float, List[int]]:
         expected = num_blocks * probabilities[i]
         chi_square += ((counts[i] - expected) ** 2) / expected
 
-    return chi_square, counts
+    df = 3
+
+    p_value = gammaincc(df / 2, chi_square / 2)
+
+    return p_value
 
 
 def compile_analysis(bits: str, language: str) -> str:
     """Run tests and return formatted result"""
     p_value_freq = frequency_test(bits)
     p_value_runs = runs_test(bits)
-    chi_square, counts = longest_run_test(bits)
+    p_value_long = longest_run_test(bits)
 
     result = (
         f"\nFor {language}:\n"
         f"Frequency test P-value: {p_value_freq}\n"
         f"Runs test P-value: {p_value_runs}\n"
-        f"Longest run test counts: {counts}\n"
-        f"Chi-square: {chi_square}\n"
+        f"Longest run test P-value: {p_value_long}\n"
     )
 
     return result
@@ -102,12 +105,14 @@ def compile_analysis(bits: str, language: str) -> str:
 
 if __name__ == "__main__":
 
-    bits = read_sequence("sequences/seq_python.txt")
+    python_bits = read_sequence("sequences/seq_python.txt")
+    java_bits = read_sequence("sequences/seq_java.txt")
+    cpp_bits = read_sequence("sequences/seq_cpp.txt")
 
     results = ""
-    results += compile_analysis(bits, "Python")
-    results += compile_analysis(bits, "Java")
-    results += compile_analysis(bits, "C++")
+    results += compile_analysis(python_bits, "Python")
+    results += compile_analysis(java_bits, "Java")
+    results += compile_analysis(cpp_bits, "C++")
 
     print(results)
 
