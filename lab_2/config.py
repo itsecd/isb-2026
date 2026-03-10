ce1cd40036cf77c0b3d2ee2a81c1993fac99edf3
zaripov-damir <damirzari@bk.ru>
2026-03-10 22:07:30 +0300

    Add config.py

---
+++ lab_2/config.py
@@ -0,0 +1,18 @@
+SIGNIFICANCE_LEVEL = 0.01
+
+RUN_BLOCK = 8
+
+EXPECTED_PROBABILITIES = (
+    0.2148,
+    0.3672,
+    0.2305,
+    0.1875,
+)
+
+SEQUENCE_FILES = {
+    "cpp": "seq_cpp.txt",
+    "java": "seq_java.txt",
+    "python": "seq_python.txt",
+}
+
+OUTPUT_PATH = "results.txt"
