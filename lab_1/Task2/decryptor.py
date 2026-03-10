4076404e72835d4094a6a74cfa9bf376a0f17dd5
KhorevKirill <babar4ik06@gmail.com>
2026-03-10 16:46:38 +0400

    Update decryptor.py

--- lab_1/Task2/decryptor.py
+++ lab_1/Task2/decryptor.py
@@ -4,6 +4,7 @@ from rus import RU
 
 
 def count_char_frequency(text):
+    """Подсчитывает частоту появления каждого символа в тексте"""
     total = len(text)
     frequency = Counter(text)
     with open('frequency.txt', 'w', encoding='utf-8') as f:
@@ -13,6 +14,7 @@ def count_char_frequency(text):
     return frequency
 
 def create_decrypt_key(frequency):
+    """Создает ключ дешифрования на основе частотного анализа"""
     sorted_chars = [char for char, count in frequency.most_common()]
     decrypt_key = {}
     for i, char in enumerate(sorted_chars):
@@ -21,14 +23,17 @@ def create_decrypt_key(frequency):
     return decrypt_key
 
 def save_key(decrypt_key):
+    """Сохраняет ключ дешифрования в файл"""
     with open('first_key.txt', 'w', encoding='utf-8') as f:
         for k, v in decrypt_key.items():
             f.write(f"{k} : {v}\n")
 
 def decrypt_text(text, decrypt_key):
-    return ''.join(decrypt_key.get(char, char) for char in text) #для каждого символа сверяет его наличие в словаре
+    """Дешифрует текст, используя ключ дешифрования"""
+    return ''.join(decrypt_key.get(char, char) for char in text)
 
 def load_right_key():
+    """агружает правильный ключ"""
     right_key = {}
     with open('right_key.txt', 'r', encoding='utf-8') as f:
         for line in f:
@@ -42,24 +47,25 @@ def load_right_key():
     return right_key
 
 def save_right_result(decrypted_text):
+    """Сохраняет правильно расшифрованный текст"""
     with open('right_result.txt', 'w', encoding='utf-8') as f:
         f.write(decrypted_text)
 
 def main():
     with open('text.txt', 'r', encoding='utf-8') as f:
         text = f.read()
-    
+
     frequency = count_char_frequency(text)
     decrypt_key = create_decrypt_key(frequency)
     save_key(decrypt_key)
-    
+
     decrypted_text = decrypt_text(text, decrypt_key)
     with open('first_result.txt', 'w', encoding='utf-8') as f:
         f.write(decrypted_text)
-    
+
     right_key = load_right_key()
     right_decrypted_text = decrypt_text(text, right_key)
     save_right_result(right_decrypted_text)
 
 if __name__ == "__main__":
-    main()
+    main() 
