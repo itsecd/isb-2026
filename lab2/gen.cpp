6396ef7b443c431d0b547a337c54883a8cd0639d
xe-n0x <bakhtinucheba860@gmail.com>
2026-03-10 19:41:00 +0400

    added 1 more bit in cpp gen

--- lab2/gen.cpp
+++ lab2/gen.cpp
@@ -10,7 +10,7 @@ int main() {
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> dist(0, 1);
-	for (int i = 0; i < 127; ++i) {
+	for (int i = 0; i < 128; ++i) {
 		f << dist(gen);
 	}
 	return 0;
