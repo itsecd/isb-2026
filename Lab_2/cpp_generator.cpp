ded8c8fa13029e8b28fc7e62ca786c22049f97f1
KhorevKirill <babar4ik06@gmail.com>
2026-03-16 00:40:02 +0400

    complite Lab_2

---
+++ Lab_2/cpp_generator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+void generate_random_num()
+{
+    srand(time(NULL));
+    std::ofstream file("output_cpp.txt");
+    if (file.is_open())
+    {
+        for (int i = 0; i < 128; i++)
+        {
+            int random_num = rand() % 2;
+            file << random_num;
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << "Не удалось сгенерировать число";
+    }
+}
+
+int main()
+{
+    generate_random_num();
+    return 0;
+}
