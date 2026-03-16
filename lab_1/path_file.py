9892eb67b9df92f614945bcaea173dab77718195
R0mmel2006 <erwinrommel1313@gmail.com>
2026-03-16 23:14:09 +0300

    Lab1

---
+++ lab_1/path_file.py
@@ -0,0 +1,10 @@
+#Task1
+INPUT_FILE = 'input_text.txt'
+OUTPUT_ENCRYPTED = 'encrypted_task1.txt'
+OUTPUT_KEY = 'key_task1.txt'
+OUTPUT_DECRYPTED = 'decrypted_task1.txt'
+
+#Task2
+INPUT_KEY = 'key_task2.txt'
+INPUT_TEXT = 'encrypted_task2.txt'
+OUTPUT_TEXT = 'decrypted_task2.txt'
