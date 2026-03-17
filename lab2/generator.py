11e627e21674d7f687a186407a366b913d846146
ARGON <165267470+ARGONfm@users.noreply.github.com>
2026-03-17 14:57:20 +0400

    Delete lab2 directory

--- lab2/generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-sequence = ''.join(str(random.randint(0, 1)) for _ in range(128))
-print("Сгенерированная последовательность (Python):")
-print(sequence)
-
-
-with open("sequence_py.txt", "w") as f:
-    f.write(sequence)
