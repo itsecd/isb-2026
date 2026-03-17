db9e3439f87e490d4e17680c2f219204936b4d8a
Барабанов И.С. <barabanov_vanechka06@mail.ru>
2026-03-17 21:37:16 +0300

    created statistics.txt and igamc caclulations written to

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "generators/cpp/CppGenerate.h"
 #include "seq_analyze.h"
 #include "filereading.h"
@@ -8,18 +9,21 @@ using namespace std;
 int main() {
 	//write_file("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\cpp\\CppGen.txt",1);
 	
+	ofstream stats_file("D:\\UniversityLabs\\inform-security-base\\lab_2\\statistics.txt");
+
+
 	// Читаем сначала С++ вектор
-	cout << "C++ generated vector\n";
+	stats_file << "C++ generated vector\n";
 	vector<int> cpp_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\cpp\\CppGen.txt");
-	seq_statistics(cpp_vec,cout);
+	seq_statistics(cpp_vec,stats_file);
 
 	// Java вектор
-	cout << "Java generated vector\n";
+	stats_file << "Java generated vector\n";
 	vector<int> java_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\java\\JavaGenerated.txt");
-	seq_statistics(java_vec,cout);
+	seq_statistics(java_vec,stats_file);
 
 	// Java вектор
-	cout << "Python generated vector\n";
+	stats_file << "Python generated vector\n";
 	vector<int> python_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\python\\PyGen.txt");
-	seq_statistics(python_vec,cout);
+	seq_statistics(python_vec,stats_file);
 }
