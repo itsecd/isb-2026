c97be51871027bfac798facc3d62ef7c005af7d0
Барабанов И.С. <barabanov_vanechka06@mail.ru>
2026-03-18 10:56:28 +0300

    added relative path go files

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -3,27 +3,28 @@
 #include "generators/cpp/CppGenerate.h"
 #include "seq_analyze.h"
 #include "filereading.h"
+#include <filesystem>
 
 using namespace std;
 
 int main() {
-	//write_file("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\cpp\\CppGen.txt",1);
+	//write_file("D:/UniversityLabs/inform-security-base/lab_2/generators/cpp/CppGen.txt",1);
 	
-	ofstream stats_file("D:\\UniversityLabs\\inform-security-base\\lab_2\\statistics.txt");
+	ofstream stats_file("../../../statistics.txt");
 
 
 	// Читаем сначала С++ вектор
 	stats_file << "C++ generated vector\n";
-	vector<int> cpp_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\cpp\\CppGen.txt");
+	vector<int> cpp_vec = read_sequence("../../../generators/cpp/CppGen.txt");
 	seq_statistics(cpp_vec,stats_file);
 
 	// Java вектор
 	stats_file << "Java generated vector\n";
-	vector<int> java_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\java\\JavaGenerated.txt");
+	vector<int> java_vec = read_sequence("../../../generators/java/JavaGenerated.txt");
 	seq_statistics(java_vec,stats_file);
 
 	// Java вектор
 	stats_file << "Python generated vector\n";
-	vector<int> python_vec = read_sequence("D:\\UniversityLabs\\inform-security-base\\lab_2\\generators\\python\\PyGen.txt");
+	vector<int> python_vec = read_sequence("../../../generators/python/PyGen.txt");
 	seq_statistics(python_vec,stats_file);
 }
