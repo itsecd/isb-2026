0304c9fa868a8fae80a749b65aca43b5af61c87b
HermanKosakian <163388133+HermanKosakian@users.noreply.github.com>
2026-03-24 03:28:00 +0400

    Delete generator_py.py

--- generator_py.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_sequence() -> str:
-    return ''.join(str(random.randint(0, 1)) for _ in range(128))
-
-if __name__ == '__main__':
-    seq = generate_sequence()
-    with open("sequence_py.txt", "w") as f:
-        f.write(seq + "\n")
