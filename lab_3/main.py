ec7d46a4de25804dcccce1e6ade5c19b433a4d25
Dun4uv <kekovka8@gmail.com>
2026-04-19 16:37:56 +0200

    Implemented decryption

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,11 +2,11 @@ import argparse
 import os
 import json
 
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, modes
+from cryptography.hazmat.decrepit.ciphers import algorithms as decrepit_algorithms
 from cryptography.hazmat.primitives import padding as sym_padding
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as asym_padding
 from cryptography.hazmat.primitives import serialization, hashes
-
 
 
 def load_settings(config_path: str = 'settings.json') -> dict:
@@ -29,21 +29,21 @@ def generate_keys(settings: dict):
     """Hybrid system key generation."""
     print("\n!!STARTING KEY GENERATION!!")
 
-    print("-Generating a 128-bit key for the SEED algorithm...")
+    print("--Generating a 128-bit key for the SEED algorithm...")
     sym_key = os.urandom(16)
 
-    print("-Generating an RSA key pair...")
+    print("--Generating an RSA key pair...")
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = private_key.public_key()
 
-    print(f"-Saving the public key in {settings['public_key']}...")
+    print(f"--Saving the public key in {settings['public_key']}...")
     with open(settings['public_key'], 'wb') as pub_out:
         pub_out.write(public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
 
-    print(f"-Saving the private key in {settings['secret_key']}...")
+    print(f"--Saving the private key in {settings['secret_key']}...")
     with open(settings['secret_key'], 'wb') as priv_out:
         priv_out.write(private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
@@ -51,47 +51,82 @@ def generate_keys(settings: dict):
             encryption_algorithm=serialization.NoEncryption()
         ))
 
-    print(f"-Symmetric key encryption and storage in {settings['symmetric_key']}...")
+    print(f"--Symmetric key encryption and storage in {settings['symmetric_key']}...")
     encrypted_sym_key = public_key.encrypt(sym_key, get_asym_padding())
 
     with open(settings['symmetric_key'], 'wb') as sym_out:
         sym_out.write(encrypted_sym_key)
 
-    print("--Key generation has been completed successfully!")
+    print("-Key generation has been completed successfully!")
 
 
 def encrypt_data(settings: dict):
     """Data encryption by a hybrid system."""
     print("\n!!STARTING DATA ENCRYPTION!!")
 
-    print("-Uploading the RSA private key...")
+    print("--Uploading the RSA private key...")
     with open(settings['secret_key'], 'rb') as pem_in:
         private_key = serialization.load_pem_private_key(pem_in.read(), password=None)
 
-    print("-Reading and decrypting the symmetric SEED key...")
+    print("--Reading and decrypting the symmetric SEED key...")
     with open(settings['symmetric_key'], 'rb') as sym_in:
         encrypted_sym_key = sym_in.read()
 
     sym_key = private_key.decrypt(encrypted_sym_key, get_asym_padding())
 
-    print(f"-Reading the source file {settings['initial_file']}...")
+    print(f"--Reading the source file {settings['initial_file']}...")
     with open(settings['initial_file'], 'rb') as f_in:
         plain_text = f_in.read()
 
     padder = sym_padding.ANSIX923(128).padder()
     padded_text = padder.update(plain_text) + padder.finalize()
 
-    print("-Data encryption using the SEED algorithm...")
+    print("--Data encryption using the SEED algorithm...")
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.SEED(sym_key), modes.CBC(iv))
+    cipher = Cipher(decrepit_algorithms.SEED(sym_key), modes.CBC(iv))
     encryptor = cipher.encryptor()
     cipher_text = encryptor.update(padded_text) + encryptor.finalize()
 
-    print(f"-Saving an encrypted file in {settings['encrypted_file']}...")
+    print(f"--Saving an encrypted file in {settings['encrypted_file']}...")
     with open(settings['encrypted_file'], 'wb') as f_out:
         f_out.write(iv + cipher_text)
 
-    print("--The encryption has been successfully completed!")
+    print("-The encryption has been successfully completed!")
+
+
+def decrypt_data(settings: dict):
+    """Decryption of data by a hybrid system."""
+    print("\n!!STARTING DATA DECRYPTION!!")
+
+    print("--Uploading the RSA private key...")
+    with open(settings['secret_key'], 'rb') as pem_in:
+        private_key = serialization.load_pem_private_key(pem_in.read(), password=None)
+
+    print("--Reading and decrypting a symmetric SEED key...")
+    with open(settings['symmetric_key'], 'rb') as sym_in:
+        encrypted_sym_key = sym_in.read()
+
+    sym_key = private_key.decrypt(encrypted_sym_key, get_asym_padding())
+
+    print(f"--Reading an encrypted file {settings['encrypted_file']}...")
+    with open(settings['encrypted_file'], 'rb') as f_in:
+        file_content = f_in.read()
+    iv = file_content[:16]
+    cipher_text = file_content[16:]
+
+    print("--Decryption of data by the SEED algorithm...")
+    cipher = Cipher(decrepit_algorithms.SEED(sym_key), modes.CBC(iv))
+    decryptor = cipher.decryptor()
+    padded_plain_text = decryptor.update(cipher_text) + decryptor.finalize()
+    unpadder = sym_padding.ANSIX923(128).unpadder()
+    plain_text = unpadder.update(padded_plain_text) + unpadder.finalize()
+
+    print(f"--Saving the result in {settings['decrypted_file']}...")
+    with open(settings['decrypted_file'], 'wb') as f_out:
+        f_out.write(plain_text)
+
+    print("-Decryption has been completed successfully!")
+
 
 def main():
     parser = argparse.ArgumentParser(description="RSA & SEED hybrid encryption")
