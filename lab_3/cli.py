d29bd47ad4dab39bd9ee13244aa64a5f3706184f
abramovivan123 <abramovivan123efhdhtyfdyhdy@gmail.com>
2026-05-05 14:55:15 +0400

    Сделал использование match/case вместо if/else в "cli.py". Добавил обработку исключений в "config_utils.py". Указал версию библиотек в "requirements.txt".

--- lab_3/cli.py
+++ lab_3/cli.py
@@ -15,43 +15,44 @@ def main():
     args = parser.parse_args()
     settings = load_config(args.config)
 
-    if args.generation:
-        print("[1/4] Генерирую AES ключ...")
-        print("[2/4] Генерирую RSA ключи...")
-        generate_keys(
-            settings["encrypted_key_file"],
-            settings["public_key_file"],
-            settings["private_key_file"],
-            int(settings["aes_key_size"])
-        )
-        print("[3/4] Сохраняю RSA ключи...")
-        print("[4/4] Сохраняю зашифрованный AES ключ...")
-        print("Готово: ключи созданы")
+    match True:
+        case _ if args.generation:
+            print("[1/4] Генерирую AES ключ...")
+            print("[2/4] Генерирую RSA ключи...")
+            generate_keys(
+                settings["encrypted_key_file"],
+                settings["public_key_file"],
+                settings["private_key_file"],
+                int(settings["aes_key_size"])
+            )
+            print("[3/4] Сохраняю RSA ключи...")
+            print("[4/4] Сохраняю зашифрованный AES ключ...")
+            print("Готово: ключи созданы")
 
-    elif args.encryption:
-        print("[1/3] Загружаю открытый RSA ключ...")
-        print("[2/3] Шифрую файл алгоритмом AES-CBC...")
-        encrypt_file(
-            settings["input_file"],
-            settings["public_key_file"],
-            settings["encrypted_key_file"],
-            settings["encrypted_file"],
-            int(settings["aes_key_size"])
-        )
-        print("[3/3] Сохраняю зашифрованный файл...")
-        print("Готово: файл зашифрован")
+        case _ if args.encryption:
+            print("[1/3] Загружаю открытый RSA ключ...")
+            print("[2/3] Шифрую файл алгоритмом AES-CBC...")
+            encrypt_file(
+                settings["input_file"],
+                settings["public_key_file"],
+                settings["encrypted_key_file"],
+                settings["encrypted_file"],
+                int(settings["aes_key_size"])
+            )
+            print("[3/3] Сохраняю зашифрованный файл...")
+            print("Готово: файл зашифрован")
 
-    elif args.decryption:
-        print("[1/3] Загружаю закрытый RSA ключ и зашифрованный AES ключ...")
-        print("[2/3] Расшифровываю файл алгоритмом AES-CBC...")
-        decrypt_file(
-            settings["encrypted_file"],
-            settings["private_key_file"],
-            settings["encrypted_key_file"],
-            settings["decrypted_file"]
-        )
-        print("[3/3] Сохраняю расшифрованный файл...")
-        print("Готово: файл расшифрован")
+        case _ if args.decryption:
+            print("[1/3] Загружаю закрытый RSA ключ и зашифрованный AES ключ...")
+            print("[2/3] Расшифровываю файл алгоритмом AES-CBC...")
+            decrypt_file(
+                settings["encrypted_file"],
+                settings["private_key_file"],
+                settings["encrypted_key_file"],
+                settings["decrypted_file"]
+            )
+            print("[3/3] Сохраняю расшифрованный файл...")
+            print("Готово: файл расшифрован")
 
 
 if __name__ == "__main__":
--- lab_3/config_utils.py
+++ lab_3/config_utils.py
@@ -2,10 +2,18 @@ import json
 
 
 def load_config(path):
-    with open(path, "r", encoding="utf-8") as file:
-        return json.load(file)
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Файл конфигурации не найден: {path}")
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Ошибка чтения JSON: {e}")
 
 
 def save_config(path, config):
-    with open(path, "w", encoding="utf-8") as file:
-        json.dump(config, file, indent=4, ensure_ascii=False)
+    try:
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(config, file, indent=4, ensure_ascii=False)
+    except OSError as e:
+        raise OSError(f"Ошибка сохранения конфигурации: {e}")
