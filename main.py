c26caf93381008ffa5f589ba6130ea55ed13bbd0
gitKoblas <Jarikpla@yandex.ru>
2026-05-05 08:19:45 +0400

    добавил дешифрование данных гибридной системой

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 
 import argparse
 import os
-from cryptography.hazmat.primitives.asymetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
-from cryptography.hazmat.primitives.asymetric import padding
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import padding as sym_padding
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymetric.rsa import RSAPrivateKey, RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
 
 
 def parser():
@@ -25,7 +25,7 @@ def parser():
     elif args.encryption is not None:
         return enc_logic()
     else:
-        pass
+        return dec_logic()
 
 def json_parser() -> dict:
     try:
@@ -35,12 +35,12 @@ def json_parser() -> dict:
     except Exception as ex:
         print(f"Ошибка!: {ex}")
 
-def get_symetric_key(byt:int) -> bytes:
+def get_symmetric_key(byt:int) -> bytes:
     """ключ для симметричного алгоритма"""
     key = os.urandom(byt)
     return key
 
-def get_asymetric_key() -> bytes:
+def get_asymmetric_key() -> bytes:
     """ключ для ассиметричного алгоритма"""
     keys = rsa.generate_private_key(
     public_exponent=65537,
@@ -50,15 +50,15 @@ def get_asymetric_key() -> bytes:
     public_key = keys.public_key()
     return private_key, public_key
 
-def write_symetric_key(symetric_key: bytes, symetric_path:str) -> None:
+def write_symmetric_key(symmetric_key: bytes, symmetric_path:str) -> None:
     """сериализация ссиметричного ключа в файл"""
     try:
-        with open(symetric_path, 'wb') as key_file:
-            key_file.write(symetric_key)
+        with open(symmetric_path, 'wb') as key_file:
+            key_file.write(symmetric_key)
     except Exception as ex:
         print(f"Ошибка!: {ex}")
 
-def write_asymetric_key(public_key:RSAPublicKey, private_key:RSAPrivateKey, public_path:str, private_path:str) -> None:
+def write_asymmetric_key(public_key:RSAPublicKey, private_key:RSAPrivateKey, public_path:str, private_path:str) -> None:
     """сериализация ассиметричных ключей в файл"""
     try:
         with open(public_path, 'wb') as public_out:
@@ -71,16 +71,16 @@ def write_asymetric_key(public_key:RSAPublicKey, private_key:RSAPrivateKey, publ
     except Exception as ex:
         print(f"Ошибка!: {ex}")
 
-def read_symetric_key(symetric_key_path:str) -> bytes:
+def read_symmetric_key(symmetric_key_path:str) -> bytes:
     """чтение ключа из файла"""
     try:
-        with open(symetric_key_path, mode='rb') as key_file: 
+        with open(symmetric_key_path, mode='rb') as key_file: 
             key = key_file.read()
             return key
     except Exception as ex:
         print(f"Ошибка!: {ex}")
 
-def read_asymetric_key(public_key_path:str, private_key_path) -> tuple:
+def read_asymmetric_key(public_key_path:str, private_key_path) -> tuple:
     """чтение RSA ключей из файлов"""
     try:
         with open(public_key_path, 'rb') as pem_in:
@@ -96,44 +96,58 @@ def read_asymetric_key(public_key_path:str, private_key_path) -> tuple:
 def read_text(initial_file_path:str) -> bytes:
     """чтение текста из файла"""
     try:
-        with open(initial_file_path, 'rb', encoding='utf-8') as f:
+        with open(initial_file_path, 'rb') as f:
             return f.read()
     except Exception as ex:
         print(f"Ошибка!: {ex}")
         return b""
     
-def write_text(text: str,  enc_file_path: str) -> None:
+def write_text(text: bytes,  enc_file_path: str) -> None:
     """запись расшифрованного текста в файл"""
     try:
-        with open(enc_file_path, 'w', encoding='utf-8') as f:
+        with open(enc_file_path, 'wb') as f:
             f.write(text)
     except Exception as ex:
         print(f"Ошибка!: {ex}")
 
-def encrypt_symetric_key(symetric_key: bytes, public_key: RSAPublicKey) -> bytes:
+def encrypt_symmetric_key(symmetric_key: bytes, public_key: RSAPublicKey) -> bytes:
     """шифрование ключа с помощью RSA-OAEP"""
-    encrypted_key = public_key.encrypt(symetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+    encrypted_key = public_key.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
     return encrypted_key
 
-def decrypt_symetric_key(encrypted_key: bytes, private_key: RSAPrivateKey) -> bytes:
+def decrypt_symmetric_key(encrypted_key: bytes, private_key: RSAPrivateKey) -> bytes:
     """дешифровка ключа"""
     return private_key.decrypt(encrypted_key,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
 
 def padding_text(text:bytes) ->bytes:
     """паддинг текста под стандарты 3DES"""
-    padder = padding.ANSIX923(64).padder()
+    padder = sym_padding.ANSIX923(64).padder()
     padded_text = padder.update(text)+padder.finalize()
     return padded_text
 
-def encrypt_text(text:bytes, symetric_key:bytes) -> bytes:
+def encrypt_text(text:bytes, symmetric_key:bytes) -> bytes:
     """шифрование текста помощью алгоритма 3DES"""
     iv = os.urandom(8) 
-    cipher = Cipher(algorithms.TripleDES(symetric_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.TripleDES(symmetric_key), modes.CBC(iv))
     encryptor = cipher.encryptor()
     padded_text = padding_text(text)
     encrypted_text = encryptor.update(padded_text) + encryptor.finalize()
     return iv + encrypted_text
 
+def decrypt_text(encrypted_data: bytes, symmetric_key: bytes) -> str:
+    """Дешифровка и удаление паддинга"""
+    try:
+        iv = encrypted_data[:8]
+        ciphertext = encrypted_data[8:]
+        cipher = Cipher(algorithms.TripleDES(symmetric_key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        padded_text = decryptor.update(ciphertext) + decryptor.finalize()
+        unpadder = sym_padding.ANSIX923(64).unpadder()
+        unpadded_text = unpadder.update(padded_text) + unpadder.finalize()
+        return unpadded_text
+    except Exception as ex:
+        print(f"Ошибка!: {ex}")
+        return ""
 
 def gen_logic() -> tuple:
     """консольный интерфейс"""
@@ -143,55 +157,55 @@ def gen_logic() -> tuple:
     key_length = int(input("Выберите длинну ключа:"))
     while(key_length != 64 and key_length != 128 and key_length != 192):
         key_length = int(input("Выберите значения из списка!\n Ваш выбор:"))
-    symetric_key = get_symetric_key(key_length // 8)
-    private_key, public_key = get_asymetric_key()
-    encrypted_key = encrypt_symetric_key(symetric_key, public_key)
+    symmetric_key = get_symmetric_key(key_length // 8)
+    private_key, public_key = get_asymmetric_key()
+    encrypted_key = encrypt_symmetric_key(symmetric_key, public_key)
     print("Ключи успешно сгенерированы")
     print("Хотите использовать стандартные параметры сериализации ключей?")
     action = str(input("Да\Нет"))
     while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
         print("Хотите использовать стандартные параметры сериализации ключей?")
         action = str(input("Да\Нет"))
-    if action == "Да" or action == "да":
-        symetric_key_path = str(input("Введите путь(имя) файла для сохранения 3DES ключа:"))
-        write_symetric_key(encrypted_key, symetric_key_path)
+    if action == "Нет" or action == "нет":
+        symmetric_key_path = str(input("Введите путь(имя) файла для сохранения 3DES ключа:"))
+        write_symmetric_key(encrypted_key, symmetric_key_path)
         public_key_path = str(input("Введите путь(имя) файла для сохранения RSAPublic ключа:"))
         private_key_path = str(input("Введите путь(имя) файла для сохранения RSAPrivate ключа:"))
-        write_asymetric_key(public_key, private_key, public_key_path, private_key_path)
+        write_asymmetric_key(public_key, private_key, public_key_path, private_key_path)
         print("Ключи успешно сохранены")
-        return symetric_key, public_key, private_key
+        return symmetric_key, public_key, private_key
     else:
         settings = json_parser()
-        write_symetric_key(encrypted_key, settings["symetric_key_path"])
-        write_asymetric_key(public_key, private_key, settings["public_key_path"], settings["private_key_path"])
+        write_symmetric_key(encrypted_key, settings["symmetric_key_path"])
+        write_asymmetric_key(public_key, private_key, settings["public_key_path"], settings["private_key_path"])
         print("Ключи успешно сохранены")
-        return symetric_key, public_key, private_key
+        return symmetric_key, public_key, private_key
 
 def enc_logic():
     """консольный интерфейс"""
+    settings = json_parser()
     print("Мастер шифровки текста")
     action = str(input("Желаете сгенерировать ключи шифрования?(Да\Нет)"))
     while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
         action = str(input("Желаете сгенерировать ключи шифрования?(Да\Нет)"))
     if action == "Да" or action == "да":
-        symetric_key, public_key, private_key = gen_logic()
+        symmetric_key, public_key, private_key = gen_logic()
     else:
         action = str(input("Ключи записаны в стандартные директории?(Да\Нет):"))
         while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
             action = str(input("Ключи записаны в стандартные директории?(Да\Нет):"))
         if action == "Да" or action == "да":
-            settings = json_parser()
-            symetric_key = read_symetric_key(settings["symetric_key_path"])
-            public_key, private_key = read_asymetric_key(settings["asymetric_key_path"])
-            symetric_key = encrypt_symetric_key(symetric_key, private_key)
+            symmetric_key = read_symmetric_key(settings["symmetric_key_path"])
+            public_key, private_key = read_asymmetric_key(settings["public_key_path"], settings["private_key_path"])
+            symmetric_key = decrypt_symmetric_key(symmetric_key, private_key)
             print("Ключи успешно загружены")
         else:
-            symetric_key_path = str(input("Введите путь(имя) файла 3DES ключа:"))
-            symetric_key = read_symetric_key(symetric_key_path)
+            symmetric_key_path = str(input("Введите путь(имя) файла 3DES ключа:"))
+            symmetric_key = read_symmetric_key(symmetric_key_path)
             public_key_path = str(input("Введите путь(имя) файла для сохранения RSAPublic ключа:"))
             private_key_path = str(input("Введите путь(имя) файла для сохранения RSAPrivate ключа:"))
-            public_key, private_key = read_asymetric_key(public_key_path, private_key_path)
-            symetric_key = encrypt_symetric_key(symetric_key, private_key)
+            public_key, private_key = read_asymmetric_key(public_key_path, private_key_path)
+            symmetric_key = decrypt_symmetric_key(symmetric_key, private_key)
             print("Ключи успешно загружены")
     action = str(input("Текст записаны в стандартные директории?(Да\Нет):"))
     while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
@@ -201,7 +215,7 @@ def enc_logic():
     else:
         initial_file_path = str(input("Введите путь(имя) к тексту:"))
         text = read_text(initial_file_path)
-    enc_text = encrypt_text(text, symetric_key)
+    enc_text = encrypt_text(text, symmetric_key)
     print("Текст успешно зашифрован")
     action = str(input("Записать хашифрованный текст в стандартные директории?(Да\Нет):"))
     while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
@@ -211,5 +225,62 @@ def enc_logic():
     else:
         enc_path = str(input("Введите путь(имя) файла для сохранения зашифрованного сообщения:"))
         write_text(enc_text, enc_path)
-    return enc_text, symetric_key
+    return enc_text, symmetric_key
     
+
+def dec_logic():
+    """консольный интерфейс"""
+    settings = json_parser()
+    print("Мастер дешифровки текста")
+    action = str(input("Ключи записаны в стандартные директории(Да\Нет)?"))
+    while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
+        action = str(input("Ключи записаны в стандартные директории(Да\Нет)?"))
+    if action == "Да" or action == "да":
+        symmetric_key = read_symmetric_key(settings["symmetric_key_path"])
+        public_key, private_key = read_asymmetric_key(settings["public_key_path"], settings["private_key_path"])
+        symmetric_key = decrypt_symmetric_key(symmetric_key, private_key)
+        print("Ключи успешно загружены")
+    else:
+        symmetric_key_path = str(input("Введите путь(имя) файла 3DES ключа:"))
+        symmetric_key = read_symmetric_key(symmetric_key_path)
+        public_key_path = str(input("Введите путь(имя) файла для сохранения RSAPublic ключа:"))
+        private_key_path = str(input("Введите путь(имя) файла для сохранения RSAPrivate ключа:"))
+        public_key, private_key = read_asymmetric_key(public_key_path, private_key_path)
+        symmetric_key = decrypt_symmetric_key(symmetric_key, private_key)
+        print("Ключи успешно загружены")
+    action = str(input("Текст записаны в стандартные директории?(Да\Нет):"))
+    while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
+        action = str(input("Ключи записаны в стандартные директории?(Да\Нет):"))
+    if action == "Да" or action == "да":
+        text = read_text(settings["encrypted_file_path"])
+    else:
+        enc_path = str(input("Введите путь(имя) к тексту:"))
+        text = read_text(enc_path)
+    text = decrypt_text(text, symmetric_key)
+    print("Текст успешно расшифрован")
+    action = str(input("Записать расшифрованный текст в стандартные директории?(Да\Нет):"))
+    while(action != "Да" and action != "Нет" and action != "да" and action != "нет"):
+        action = str(input("Записать расшифрованный текст в стандартные директории?:"))
+    if action == "Да" or action == "да":
+        write_text(text, settings["decrypted_file_path"])
+    else:
+        enc_path = str(input("Введите путь(имя) файла для сохранения зашифрованного сообщения:"))
+        write_text(text, enc_path)
+    return text, symmetric_key
+
+
+def main():
+    parser()
+    action = str(input("Выберите действие(enc/dec/gen/exit):"))
+    while(action != "exit"):
+        if action == "enc":
+            enc_logic()
+        elif action == "dec":
+            dec_logic()
+        elif action == "gen":
+            gen_logic()
+        action = str(input("Выберите действие(enc/dec/gen/exit):"))
+    return 0
+
+if __name__ == "__main__":
+    main()
