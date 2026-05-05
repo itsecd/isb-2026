e8a61c0f32d1587d8f5e641b48df7a99e4e41358
Chuqaz <kardanovmr52@mail.ru>
2026-05-05 16:02:47 +0400

    Update asymmetric_keys_interaction.py

--- asymmetric_keys_interaction.py
+++ asymmetric_keys_interaction.py
@@ -1,8 +1,11 @@
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 
-def enc_sym_key(public_key,sym_key)->bytes:
-    """Шифровка симметричного ключа"""
+def enc_sym_key(public_key:RSAPublicKey,sym_key:bytes)->bytes:
+    """
+    Шифровка симметричного ключа
+    """
     enc_sym_key=public_key.encrypt(
         sym_key, 
         padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -11,8 +14,10 @@ def enc_sym_key(public_key,sym_key)->bytes:
                      )
     return enc_sym_key
 
-def dec_sym_key(private_key,enc_sym_key)->bytes:
-    """Дешифровка симметричного ключа"""
+def dec_sym_key(private_key:RSAPrivateKey,enc_sym_key:bytes)->bytes:
+    """
+    Дешифровка симметричного ключа
+    """
     dec_sym_key=private_key.decrypt(enc_sym_key,
                         padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                      algorithm=hashes.SHA256(),
