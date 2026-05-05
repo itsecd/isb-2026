aea1c0f9bd6c4aed99c24323afd7f7c2bc735584
bbtoji <mrmagistrr@gmail.com>
2026-05-05 10:46:13 +0400

    create branch lab3

--- lab_2/const.py
+++
@@ -1,7 +0,0 @@
-BLOCK_SIZE = 128
-FILES = [
-    "bits_cpp.txt",
-    "bits_python.txt",
-    "bits_java.txt",
-]
-OUTPUT_FILE = "test_results.txt"
--- lab_2/prng.py
+++
@@ -1,5 +0,0 @@
-import random
-
-with open("bits_python.txt", "w") as f:
-    for _ in range(128):
-        f.write(str(random.randint(0,1)))
