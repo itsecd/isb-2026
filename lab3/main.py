caf5df5f30e4169acbbc8814b665db3845d12499
daria <uraskina.darya@bk.ru>
2026-05-12 20:55:20 +0300

    added match/case

--- lab3/main.py
+++ lab3/main.py
@@ -31,74 +31,82 @@ def main():
 
     except FileNotFoundError:
         print("Settings file was not found")
+        return
     except Exception as error:
         print(f"Error has occurred: {error}")
-    
+        return
+
+
     try:
-        if args.generation is not None:
-            print("1. Keys generation mode")
-            
-            key = generate_symmetric_key()
-            print("1.1 Key for the symmetric algorithm has been generated")
+        match (args.generation, args.encryption, args.decryption):
+            case (str(), _, _):
+                print("1. Keys generation mode")
+                
+                key = generate_symmetric_key()
+                print("1.1 Key for the symmetric algorithm has been generated")
 
-            private_key, public_key = generate_asymmetric_keys()
-            print("1.2 Keys for asymmetric algorithm have been generated")
+                private_key, public_key = generate_asymmetric_keys()
+                print("1.2 Keys for asymmetric algorithm have been generated")
 
-            write_public_key(public_key, public_key_file)
-            print("1.3.1 Public key is serialized in .pem file")
+                write_public_key(public_key, public_key_file)
+                print("1.3.1 Public key is serialized in .pem file")
 
-            write_private_key(private_key, secret_key_file)
-            print("1.3.2 Private key is serialized in .pem file")
+                write_private_key(private_key, secret_key_file)
+                print("1.3.2 Private key is serialized in .pem file")
 
-            encrypt_key = encrypt_symmetric_key(key, public_key)
-            print("1.4.1 Symmetric encryption key is encrypted with public key")
+                encrypt_key = encrypt_symmetric_key(key, public_key)
+                print("1.4.1 Symmetric encryption key is encrypted with public key")
 
-            write_symmetric_key(encrypt_key, symmetric_key_file)
-            print("1.4.2 Encrypted symmetric encryption key is saved in .txt file")
-            
-
-        elif args.encryption is not None:
-            print("2. Data encryption mode")
-
-            content = read_symmetric_key(symmetric_key_file)
-            print("2.1.1 Encrypted symmetric key is read from file")
-
-            private_key = read_private_pem(secret_key_file)
-            print("2.1.2 Private key for decrypting symmetric key is read from file")
-
-            key = decrypt_symmetric_key(content, private_key)
-            print("2.1.3 Symmetric key has been decrypted")
-
-            text_bytes = read_text_file(initial_file)
-            print("2.2.1 Text for encryption is read from file")
-
-            iv, c_text = encrypt_text(text_bytes, key)
-            print("2.2.2 Text was encrypted using SM4 algorithm")
-
-            write_encrypt_text(encrypted_file, iv, c_text)
-            print("2.2.3 Encrypted text was written to file")
+                write_symmetric_key(encrypt_key, symmetric_key_file)
+                print("1.4.2 Encrypted symmetric encryption key is saved in .txt file")
             
 
-        else:
-            print("3. Data decryption mode")
+            case (_, str(), _):
+                print("2. Data encryption mode")
 
-            content = read_symmetric_key(symmetric_key_file)
-            print("3.1.1 Encrypted symmetric key is read from file")
+                content = read_symmetric_key(symmetric_key_file)
+                print("2.1.1 Encrypted symmetric key is read from file")
 
-            private_key = read_private_pem(secret_key_file)
-            print("3.1.2 Private key for decrypting symmetric key is read from file")
+                private_key = read_private_pem(secret_key_file)
+                print("2.1.2 Private key for decrypting symmetric key is read from file")
 
-            key = decrypt_symmetric_key(content, private_key)
-            print("3.1.3 Symmetric key has been decrypted")
+                key = decrypt_symmetric_key(content, private_key)
+                print("2.1.3 Symmetric key has been decrypted")
 
-            iv, c_text = read_encrypt_text(encrypted_file)
-            print("3.2.1 Encrypted text is read from file")
+                text_bytes = read_text_file(initial_file)
+                print("2.2.1 Text for encryption is read from file")
 
-            dc_text = decrypt_text(iv, c_text, key)
-            print("3.2.2 Text has been decrypted")
+                iv, c_text = encrypt_text(text_bytes, key)
+                print("2.2.2 Text was encrypted using SM4 algorithm")
 
-            write_decrypt_text(decrypted_file, dc_text)
-            print("3.2.3 Decrypted text was written to file")
+                write_encrypt_text(encrypted_file, iv, c_text)
+                print("2.2.3 Encrypted text was written to file")
+            
+
+            case (_, _, str()):
+                print("3. Data decryption mode")
+
+                content = read_symmetric_key(symmetric_key_file)
+                print("3.1.1 Encrypted symmetric key is read from file")
+
+                private_key = read_private_pem(secret_key_file)
+                print("3.1.2 Private key for decrypting symmetric key is read from file")
+
+                key = decrypt_symmetric_key(content, private_key)
+                print("3.1.3 Symmetric key has been decrypted")
+
+                iv, c_text = read_encrypt_text(encrypted_file)
+                print("3.2.1 Encrypted text is read from file")
+
+                dc_text = decrypt_text(iv, c_text, key)
+                print("3.2.2 Text has been decrypted")
+
+                write_decrypt_text(decrypted_file, dc_text)
+                print("3.2.3 Decrypted text was written to file")
+
+
+            case _:
+                print("Error: wrong argument")
              
     except Exception as error:
         print(f"Error has occurred: {error}")
