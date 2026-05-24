7d8cb2ad8d8a0711f42e57136bdd175d698ca824
andruyxaParavoz <andrey.malahov.679@gmail.com>
2026-05-24 15:25:45 +0400

    implemented database logic

---
+++ lab4/DB.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db_path="database.db"):
+        self.db_path = db_path
+        self.init_db()
+
+    
+    def connection(self):
+        return sqlite3.connect(self.db_path)
+    
+
+    def init_db(self):
+        with self.connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT UNIQUE NOT NULL,
+                    password_hash TEXT NOT NULL,
+                    salt TEXT,
+                    is_safe INTEGER NOT NULL
+                )
+            """)
+            conn.commit()
+
+
+    def add_user(self, username, password_hash, salt, is_safe):
+        with self.connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO users (username, password_hash, salt, is_safe) VALUES (?, ?, ?, ?)",
+                (username, password_hash, salt, int(is_safe))
+            )
+            conn.commit()
+
+
+    def fetch_user(self, username):
+        with self.connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT password_hash, salt, is_safe FROM users WHERE username = ?", (username,))
+            return cursor.fetchone()
