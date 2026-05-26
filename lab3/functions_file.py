1c5ad135eaaf54f1b2fb8f7c8825ad6b3a116b63
KarginaIrina <ikargina06@mail.ru>
2026-05-26 16:58:59 +0400

    Create functions_file.py

---
+++ lab3/functions_file.py
@@ -0,0 +1,67 @@
+def read_text_file(initial_file):
+        """
+        считывание исходного текста из файла и преобразование его в байты для шифрования
+
+        аргументы: путь к исходному текстовому файл
+
+        возвращает: начальный текст в байтах
+
+        """
+        try:
+                with open(initial_file, "r", encoding="utf-8") as file:
+                        text = file.read()
+                return text.encode('utf-8')
+
+        except FileNotFoundError:
+                raise FileNotFoundError(f"Нет такого файла {initial_file}")
+        
+
+
+def write_encrypt_text(encrypted_file, iv, etext_in_bytes):
+        """
+        сохранение зашифрованного текста и вектора инициализации в файл
+        аргументы: 
+                encrypted_file: путь к файлу для сохранения зашифрованного текста
+                iv: вектор инициализации (16 случайных байтов)
+                etext_in_bytes: зашифрованный текст в байтах
+        возвращает: ничегошеньки
+        """
+
+        with open(encrypted_file, "wb") as file:
+                file.write(iv + etext_in_bytes)
+
+
+def read_encrypt_text(encrypted_file):
+        """
+        считывание зашифрованного текста из файла и разделение текста и вектора инициализации для расшифровки
+        аргументы: 
+                encrypted_file: путь к файлу с зашифрованным текстом
+                
+        возвращает:
+                tuple(iv, etext_in_bytes): кортеж с вектором инициализации (16 случайных байтов) и зашифрованным текстом в байтах  
+        """
+
+        try:
+                with open(encrypted_file, 'rb') as file: 
+                        content = file.read()
+
+                iv = content[:16]
+                etext_in_bytes = content[16:]
+
+                return iv, etext_in_bytes
+
+        except FileNotFoundError:
+                raise FileNotFoundError(f"Такой файл мы не находили {encrypted_file}")
+
+
+def write_decrypt_text(decrypted_file_in_bytes, decrypted_file):
+        """
+        сохранение расшифрованного текста в файл
+        аргументы: 
+                decrypted_file_in_bytes: путь к файлу для сохранения расшифрованного текста, в виде строки
+                decrypted_file: расшифрованный текст без заполнения в байтах
+        возвращает: совсем ничего
+        """
+        
+        with open(decrypted_file_in_bytes, 'wb') as file: 
+                file.write(decrypted_file)
