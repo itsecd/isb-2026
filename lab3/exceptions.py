b39326a4455327a1beb14042f03fcd226f6f626b
6212 Козленко Екатерина Игоревна <bla6bla6bla6bla6@gmail.com>
2026-05-27 13:01:50 +0400

    Add files via upload

---
+++ lab3/exceptions.py
@@ -0,0 +1,45 @@
+"""
+Пользовательские исключения системы.
+"""
+
+
+class CryptoSystemError(Exception):
+    """
+    Базовое исключение криптографической системы.
+    """
+    pass
+
+
+class FileProcessingError(CryptoSystemError):
+    """
+    Ошибка обработки файла.
+    """
+    pass
+
+
+class KeyGenerationError(CryptoSystemError):
+    """
+    Ошибка генерации ключей.
+    """
+    pass
+
+
+class EncryptionError(CryptoSystemError):
+    """
+    Ошибка шифрования.
+    """
+    pass
+
+
+class DecryptionError(CryptoSystemError):
+    """
+    Ошибка дешифрования.
+    """
+    pass
+
+
+class KeyLoadError(CryptoSystemError):
+    """
+    Ошибка загрузки ключа.
+    """
+    pass
