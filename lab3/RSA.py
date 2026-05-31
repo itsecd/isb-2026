6a84e9049d2063efbcdfd7f824615b58cccbab84
faibyy <deniskornilov06@mail.ru>
2026-05-31 19:12:06 +0400

    RSA encryption

--- lab3/RSA.py
+++ lab3/RSA.py
@@ -1,10 +1,11 @@
-import key_generation
+import symmetric
 
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.serialization import (
     load_pem_public_key,
     load_pem_private_key,
 )
+from cryptography.hazmat.primitives.asymmetric import padding as asym_padding
 
 def serialize_public_key(public_key, public_pem_path: str):
     """
@@ -63,7 +64,7 @@ def deserialize_public_key(public_pem_path: str):
     except FileNotFoundError as e:
         raise FileNotFoundError(f"Ошибка сохранения файла: {e}")
 
-def deserialize_private_key(private_key, private_pem_path: str):
+def deserialize_private_key(private_pem_path: str):
     """
     Save private key as a .pem file
     Args:
@@ -78,3 +79,40 @@ def deserialize_private_key(private_key, private_pem_path: str):
         return load_pem_private_key(private_bytes)
     except OSError as e:
         raise OSError(f"Ошибка сохранения файла: {e}")
+
+def encrypt_symmetric_key(public_key_path, symmetric_key):
+    """
+    Encrypting symmetric key with RSA private key
+    Args:
+        public_key_path(str): Path to oublic key
+        symmetric_key(str): symmetric key
+    Returns: 
+        encrypted_key(bytes): encrypted sym key
+    """
+    public_key = deserialize_public_key (public_key_path)
+    encrypted_key = public_key.encrypt(
+        symmetric_key,
+        asym_padding.OAEP(
+            mgf=asym_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None))
+    return encrypted_key
+
+def decrypt_symmetric_key(private_key_path, encrypted_key_path):
+    """
+    Decrypting symmetric key with RSA private key
+    Args:
+        private_key_path(str): Path to private key
+        encrypted_key_path(str): Path to encrypted symmetric key
+    Returns: 
+        decrypted_key(bytes): decrypted sym key
+    """
+    private_key = deserialize_private_key (private_key_path)
+    encrypted_key = symmetric.deserialize_encrypted_key (encrypted_key_path)
+    decrypted_key = private_key.decrypt(
+        encrypted_key,
+        asym_padding.OAEP(
+            mgf=asym_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None))
+    return decrypted_key
