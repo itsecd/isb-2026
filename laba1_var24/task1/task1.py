904acae6730c3dfb81adfc689c79f1c405282a5c
SemyonSuper777 <semencurakov@gmail.com>
2026-06-01 18:00:17 +0400

    6211 Чураков Семён Сергеевич Лаб 1 Вар 24

---
+++ laba1_var24/task1/task1.py
@@ -0,0 +1,90 @@
+import argparse
+
+import pandas as pd
+
+def read_text(filename: str) -> str:
+    """ function for read from file """
+    try:
+        with open(filename, encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found")
+        exit(2)
+    except PermissionError:
+        print(f"Premision denied")
+        exit(2)
+    except Exception as e:
+        print(e)
+        exit(2)
+
+    
+def encode_polibius(text: str, key: pd.DataFrame) -> str:
+    """ encode text function """
+    text = text.upper()
+    rows, cols = key.shape
+    map = {}
+    for i in range(rows):
+        for j in range(cols):
+            cell = key.iloc[i, j]
+            if cell and str(cell).strip():
+                map[cell] = f"{i+1}{j+1}"
+    result = ""
+    for char in text:
+        if char in map:
+            result+=map[char]
+        elif char == ' ':
+            result+= ' '
+        else:
+            result+= char 
+    return result
+
+def decode_polibius(text: str, key: pd.DataFrame) -> str:
+    """decode function"""
+    rows, cols = key.shape
+    map = {}
+    for i in range(rows):
+        for j in range(cols):
+            cell = key.iloc[i, j]
+            if cell and str(cell).strip():
+                map[f"{i+1}{j+1}"] = cell
+    
+    result = ""
+    i = 0
+    while i < len(text):
+        if text[i] == ' ':
+            result += ' '
+            i += 1
+        elif i + 1 < len(text) and text[i:i+2] in map:  
+            result += map[text[i:i+2]]
+            i += 2
+        else:
+            result += text[i]
+            i += 1
+    return result
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Polibius encode')
+    parser.add_argument('--input_file', required=True, help='file with text')
+    parser.add_argument('--key', required=True, help='key for encode')
+    parser.add_argument('--output_file', required=True, help='file for encode text')
+    args = parser.parse_args()
+    text = read_text(args.input_file)
+    key = pd.read_excel(args.key)
+    try:
+        with open(args.output_file, "w", encoding="utf-8") as f:
+            f.write(encode_polibius(text, key))    
+            print(f"Your text is encode")
+    except PermissionError:
+        print(f"Permission denied")
+        exit(2)
+    except Exception as e:
+        print(e)
+        exit(2)
+    encode_text = encode_polibius(text, key) # check, that decode work
+    decode_text = decode_polibius(encode_text, key)
+    print(decode_text)
+
+        
+if __name__ == "__main__":
+    main()
