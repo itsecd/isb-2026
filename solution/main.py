d758241c32e88d8f9cccd64c91695c6e863bf2de
gitKoblas <Jarikpla@yandex.ru>
2026-06-01 15:01:35 +0400

    Added send/receive messege methods

--- solution/main.py
+++ solution/main.py
@@ -1,13 +1,27 @@
 import hmac
 import hashlib
+import json
 
-def create_hmac(key: str, text: str) -> str:
-    hmac_hash = hmac.new(key.encode('utf-8') , text.encode('utf-8') , hashlib.sha256).hexdigest()
+def create_hmac(key: str, data: str) -> str:
+    hmac_hash = hmac.new(key.encode('utf-8') , data.encode('utf-8') , hashlib.sha256).hexdigest()
     return hmac_hash
 
-def verify_hmac(key:str, text: str, hmac_hash: str) -> bool:
-    true_hmac = create_hmac(key, text)
+def verify_hmac(key:str, data: str, hmac_hash: str) -> bool:
+    true_hmac = create_hmac(key, data)
     return hmac.compare_digest(true_hmac, hmac_hash)
+
+def send_message(data:str, hmac_hash: str, file_path:str) -> None:
+    message = {
+        "data": data,
+        "hmac_hash": hmac_hash,
+    }
+    write_data(json.dumps(message, ensure_ascii=False, indent=2), file_path)
+
+def receive_message(key: str, file_path: str) -> bool:
+    message = json.loads(read_data(file_path))
+    data = message["data"]
+    hmac_hash = message["hmac_hash"]
+    return verify_hmac(key, data, hmac_hash)
 
 def read_data(data_path:str) -> str:
     """
