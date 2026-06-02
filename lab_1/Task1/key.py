62769328d05124916cc68bce4b24c7ed72f91ae4
pl4tz <platontonnikov8@gmail.com>
2026-06-02 15:45:58 +0400

    правки

--- lab_1/Task1/key.py
+++ lab_1/Task1/key.py
@@ -1,15 +1,31 @@
 from Ru import ALPHABET, ALPHABET_LEN
 
 
-def generate_caesar_decrypt_key(shift=10):
-    """Генерирует словарь для расшифровки шифра Цезаря"""
-    decrypt_key = {}
+SHIFT = 10
+
+
+def generate_encrypt_key(shift=SHIFT):
+    """Генерирует ключ для шифрования шифром Цезаря"""
+    encrypt_key = {}
 
     for i, original_char in enumerate(ALPHABET):
         encrypted_index = (i + shift) % ALPHABET_LEN
         encrypted_char = ALPHABET[encrypted_index]
+        encrypt_key[original_char] = encrypted_char
+
+    return encrypt_key
+
+
+def generate_decrypt_key(shift=SHIFT):
+    """Генерирует ключ для расшифровки шифра Цезаря"""
+    encrypt_key = generate_encrypt_key(shift)
+    decrypt_key = {}
+
+    for original_char, encrypted_char in encrypt_key.items():
         decrypt_key[encrypted_char] = original_char
 
     return decrypt_key
 
-decrypt_key = generate_caesar_decrypt_key(shift=10)
+
+encrypt_key = generate_encrypt_key()
+decrypt_key = generate_decrypt_key()
