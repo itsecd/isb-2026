5ca36ce9c59cece52b2def3473cf5cd92fa859e7
pl4tz <platontonnikov8@gmail.com>
2026-06-02 15:46:36 +0400

    правки

--- lab_1/Task1/decryptor.py
+++ lab_1/Task1/decryptor.py
@@ -1,23 +1,28 @@
 from key import decrypt_key
 
 
-def  decrypt_text(text, decrypt_key_dict):
-    """Дешифрует текст с помощью словаря замены"""
-    result = ''
+def decrypt_text(text, decrypt_key_dict):
+    """Расшифровывает текст с помощью словаря замены"""
+    result = ""
+
     for char in text:
         if char in decrypt_key_dict:
             result += decrypt_key_dict[char]
         else:
             result += char
+
     return result
 
 
 def main():
-    with open('encrypted_text.txt', 'r', encoding='utf-8') as f:
+    with open("encrypted_text.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
-    text = text.upper()
     decrypted_text = decrypt_text(text, decrypt_key)
+
+    with open("decrypted_text.txt", "w", encoding="utf-8") as f:
+        f.write(decrypted_text)
+
     print(decrypted_text)
 
 
